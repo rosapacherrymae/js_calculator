454360da9d937774e88414e69139c00d7801bfb7
your_username <your_email@gmail.com>
2026-06-29 14:02:16 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function calculation() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    //Calculate Sum
+    const sum = number1 + number2;
+
+    //Display the results
+    document.getElementById('result').textContent= 'The sum is ' +sum;
+}
