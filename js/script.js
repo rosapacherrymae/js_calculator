47c1c3074d13fef64ab750049c53eec57d2d33c2
rosapacherrymae <cherrymaerosapa3@gmail.com>
2026-07-01 14:15:22 +0800

    added 3rd number

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 function calculation() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
 
     const number1 = parseFloat(num1);
     const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
 
     //Calculate Sum
-    const sum = number1 + number2;
+    const sum = number1 + number2 + number3;
 
     //Display the results
     document.getElementById('result').textContent= 'The sum is ' +sum;
+}
+
+function product() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const num3 = document.getElementById('num3').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
+    
+    //Calculate Product
+    const product = number1 * number2 * number3;
+
+    //Display the results
+    document.getElementById('result').textContent= 'The product is ' +product;
 }
